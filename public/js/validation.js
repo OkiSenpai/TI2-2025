5f193e83d338af98806994ae7520478e21d31f90
OmerAslani <aslaniomer21@gmail.com>
2025-05-02 20:36:38 +0200

    js better

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,20 +1,30 @@
+
+
+
+
 function verificationDeInput() {
     const btn = document.getElementById("btn");
 
-    const nom = document.getElementById("nomID");
-    const prenom = document.getElementById("prenomID");
-    const email = document.getElementById("emailID");
-    const tel = document.getElementById("nbPortablelID");
-    const codePostal = document.getElementById("codePostal");
-    const message = document.getElementById("messages");
+
+    const nomError = document.getElementById("nomError");
+    const prenomError = document.getElementById("prenomError");
+    const emailError = document.getElementById("emailError");
+    const telephoneError = document.getElementById("telephoneError");
+    const messageError = document.getElementById("messageError");
+    const codePostalErro = document.getElementById("codePostalError");
+
+
+
+    const emailRegex = /^([a-zA-Z0-9.-_]+)@([a-z0-9]+).([a-z]{2,3})$/;
+    const telRegex = /^((04)|\+324)([0-9]){8}$/;
+    const messageRegex = /^([a-zA-Z0-9 \+ \/ -])+$/;
+    const postRegex = /^[0-9]{4}$/;
+
+    const inputMessage = document.querySelector("#messages")
     const messageCounter = document.getElementById("messageCounter")
 
-    const messageLen = message.value.length;
-    console.log(messageLen);
-
-
-    message.addEventListener("input", function () {
-        const messageLen = message.value.length;
+    inputMessage.addEventListener("input", function () {
+        const messageLen = inputMessage.value.length;
         messageCounter.textContent = `${messageLen} / 300`;
 
         if (messageLen > 300) {
@@ -27,92 +37,98 @@ function verificationDeInput() {
     });
 
 
-    btn.addEventListener("click", function (e) {
+    btn.addEventListener('click', function (e) {
         let isValid = true;
 
-        const emailRegex = /^([a-zA-Z0-9.-_]+)@([a-z0-9]+).([a-z]{2,3})$/;
-        const telRegex = /^((04)|\+324)([0-9]){8}$/;
-        const postRegex = /^[0-9]{4}$/;
-        const messageRegex = /^([a-zA-Z0-9 \+ \/ -])+$/;
+        const inputName = document.querySelector("#nomID").value.trim();
+        const inputPrenom = document.querySelector("#prenomID").value.trim();
+        const inputEmail = document.getElementById("emailID").value.trim();
+        const inputTel = document.querySelector("#nbPortablelID").value.trim();
+        const inputMessage = document.querySelector("#messages").value.trim();
+        const inputCodePostal = document.querySelector("#codePostal").value.trim();
 
 
-        if (nom.value.trim() === "") {
-            nom.value = "";
-            nom.placeholder = "Nom est vide";
-            nom.style.borderColor = "red";
-            nom.style.color = "red";
+        if (inputName === "") {
+            nomError.textContent = "Nom est vide";
+            nomError.style.color = "red";
             isValid = false;
         } else {
-            nom.style.borderColor = "";
-            nom.style.color = "";
-            nom.placeholder = "";
+            nomError.textContent = "";
+        }
+
+        if (inputPrenom === "") {
+            prenomError.textContent = "Prénom est vide";
+            prenomError.style.color = "red";
+            isValid = false;
+        } else {
+            prenomError.textContent = "";
         }
 
 
-        if (prenom.value.trim() === "") {
-            prenom.value = "";
-            prenom.placeholder = "Prénom est vide";
-            prenom.style.borderColor = "red";
-            prenom.style.color = "red";
+
+
+        if (inputCodePostal === "") {
+            codePostalErro.textContent = "CodePostal est vide";
+            codePostalErro.style.color = "red";
+            isValid = false;
+        } else if (!postRegex.test(inputCodePostal)) {
+            codePostalErro.textContent = "CodePostal n'est pas valide";
+            codePostalErro.style.color = "red";
             isValid = false;
         } else {
-            prenom.style.borderColor = "";
-            prenom.style.color = "";
-            prenom.placeholder = "";
+            codePostalErro.textContent = "";
         }
 
-        if (!emailRegex.test(email.value.trim())) {
-            email.value = "";
-            email.placeholder = "Email invalide";
-            email.style.borderColor = "red";
-            email.style.color = "red";
+
+
+
+        if (inputEmail === "") {
+            emailError.textContent = "Email est vide";
+            emailError.style.color = "red";
+            isValid = false;
+        } else if (!emailRegex.test(inputEmail)) {
+            emailError.textContent = "Email n'est pas valide";
+            emailError.style.color = "red";
             isValid = false;
         } else {
-            email.style.borderColor = "";
-            email.style.color = "";
-            email.placeholder = "";
+            emailError.textContent = "";
         }
 
-        if (!telRegex.test(tel.value.trim())) {
-            tel.value = "";
-            tel.placeholder = "Téléphone invalide";
-            tel.style.borderColor = "red";
-            tel.style.color = "red";
+
+        if (inputTel === "") {
+            telephoneError.textContent = "Téléphone est vide";
+            telephoneError.style.color = "red";
+            isValid = false;
+        } else if (!telRegex.test(inputTel)) {
+            telephoneError.textContent = "Téléphone n'est pas valide";
+            telephoneError.style.color = "red";
             isValid = false;
         } else {
-            tel.style.borderColor = "";
-            tel.style.color = "";
-            tel.placeholder = "";
+            telephoneError.textContent = "";
         }
 
-        if (!postRegex.test(codePostal.value.trim())) {
-            codePostal.value = "";
-            codePostal.placeholder = "Code postal invalide";
-            codePostal.style.borderColor = "red";
-            codePostal.style.color = "red";
-            isValid = false;
-        } else {
-            codePostal.style.borderColor = "";
-            codePostal.style.color = "";
-            codePostal.placeholder = "";
-        }
 
-        if (!messageRegex.test(message.value.trim()) || message.value.length > 300) {
-            message.value = "";
-            message.placeholder = "Message invalide";
-            message.style.borderColor = "red";
-            message.style.color = "red";
+        if (inputMessage === "") {
+            messageError.textContent = "Message est vide";
+            messageError.style.color = "red";
             isValid = false;
-        } else {
-            message.style.borderColor = "";
-            message.style.color = "";
-            message.placeholder = "";
+        } else if (!messageRegex.test(inputMessage) || inputMessage.length > 300) {
+            messageError.textContent = "Message n'est pas valide";
+            messageError.style.color = "red";
+            isValid = false;
+
+        }
+        else {
+            messageError.textContent = "";
+
         }
 
         if (!isValid) {
             e.preventDefault();
         }
     });
+    console.log();
+
 }
 
 verificationDeInput();
